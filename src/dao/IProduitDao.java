c421ac18a5905e12cfde6dd2f53ba98b02fa6f4d
Ismail BARKANI <ismail.barkani@um5s.net.ma>
2019-11-17 00:19:06 +0100

    first commit

---
+++ src/dao/IProduitDao.java
@@ -0,0 +1,11 @@
+package dao;
+
+import dao.entities.Produit;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface IProduitDao {
+
+
+}
---
+++ src/dao/SingletonConnection.java
@@ -0,0 +1,24 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class SingletonConnection {
+
+    private static Connection myConnection;
+
+
+    static{
+        try {
+            Class.forName("com.mysql.jdbc.Driver");// pour charger le driver JDBC
+            myConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306//DB_CATAL","root","");
+        }catch(ClassNotFoundException | SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+    public Connection getMyConnection(){
+        return myConnection;
+    }
+}
