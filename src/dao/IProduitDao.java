5367861108256203c43fc833249eefa1357009de
Ismail BARKANI <ismail.barkani@um5s.net.ma>
2019-11-17 00:35:52 +0100

    Data Access Object et implimentation des methode

--- src/dao/IProduitDao.java
+++ src/dao/IProduitDao.java
@@ -3,9 +3,12 @@ package dao;
 import dao.entities.Produit;
 
 import java.sql.SQLException;
-import java.util.List;
+import java.util.ArrayList;
 
 public interface IProduitDao {
+
+    public Produit save(Produit p)throws SQLException;
+    public ArrayList<Produit> chercherParMC(String mc)throws SQLException;
 
 
 }
